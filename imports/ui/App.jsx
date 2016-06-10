0dd3e4738f1fe548c63bcbdd1981906acfcef433
Jethro Ordaneza <jet.oneza@gmail.com>
2016-06-10 14:44:50 +0800

    Integrate Semantic-UI

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -4,9 +4,18 @@ class App extends Component {
   render() {
     return (
         <div className="app">
-          <header>
-            <h1>SMS</h1>
-          </header>
+          <h3 className="ui center aligned header">Contacts
+            <div className="ui green label">Add</div>
+          </h3>
+
+          <div className="ui text container">
+            <div className="ui segments">
+              <div className="ui segment">John Doe</div>
+              <div className="ui segment">Micasa S. Sucasa</div>
+              <div className="ui segment">Chuck Norris</div>
+              <div className="ui segment">Bruce Lee</div>
+            </div>
+          </div>
         </div>
     );
   }
